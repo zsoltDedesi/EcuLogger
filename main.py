58c70563688823e2ead8bfa810aee1fe200b8a9f
zsoltDedesi <syncler91@gmail.com>
2022-05-11 20:30:46 +0200

    fix library import problem

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+
+
+
+
+
 def main():
     pass
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
